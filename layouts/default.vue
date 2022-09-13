4db2dee2b3f27e8a998e775ccb8aa6e4c5465e4c
Kene Arionye <kenechiaugustine@gmail.com>
2022-09-13 05:18:08 +0100

    modified main page

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <AppHeader />
+        <Nuxt />
+    </div>
+</template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h2>Welcome to the best dad jokes app on the web</h2>
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'IndexPage'
-})
+<script>
+export default {
+    head() {
+        return {
+            title: "Welcome to the best dad jokes app",
+            meta: [
+                {
+                    hid: "description",
+                    meta: "description",
+                    content: "Best dad jokes",
+                },
+            ],
+        };
+    },
+};
 </script>
