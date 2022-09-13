d4ac65195d4f919ec294a2d6452cba73791a6ce8
Kene Arionye <kenechiaugustine@gmail.com>
2022-09-13 08:17:07 +0100

    fetched dad jokes from external api

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,11 +1,33 @@
 <template>
     <div>
-        jokes
+        <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+import Joke from '~/components/Joke.vue';
+
 export default {
+    data() {
+        return {
+            jokes: []
+        };
+    },
+    async created() {
+        const config = {
+            headers: {
+                "Accept": "application/json"
+            }
+        };
+        try {
+            const res = await axios.get("https://icanhazdadjoke.com/search", config);
+            this.jokes = res.data.results;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    },
     head() {
         return {
             title: "Dad jokes",
@@ -18,6 +40,7 @@ export default {
             ],
         };
     },
+    components: { Joke }
 }
 </script>
 
