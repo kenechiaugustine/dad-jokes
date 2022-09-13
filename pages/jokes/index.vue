3b9a39949864196d938539acba45e90d9b67e41d
Kene Arionye <kenechiaugustine@gmail.com>
2022-09-13 09:15:27 +0100

    implemented search func

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <SearchJokes v-on:search-text="searchText" />
         <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
     </div>
 </template>
@@ -7,6 +8,7 @@
 <script>
 import axios from 'axios'
 import Joke from '~/components/Joke.vue';
+import SearchJokes from '~/components/SearchJokes.vue';
 
 export default {
     data() {
@@ -28,6 +30,24 @@ export default {
             console.log(error);
         }
     },
+
+    methods: {
+        async searchText(text) {
+            const config = {
+                headers: {
+                    "Accept": "application/json"
+                }
+            };
+            try {
+                const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config);
+                this.jokes = res.data.results;
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+    },
+
     head() {
         return {
             title: "Dad jokes",
@@ -40,7 +60,7 @@ export default {
             ],
         };
     },
-    components: { Joke }
+    components: { Joke, SearchJokes }
 }
 </script>
 
