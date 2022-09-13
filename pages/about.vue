c07b2172f3eb7fb6b1ab98d6a95b24170532d548
Kene Arionye <kenechiaugustine@gmail.com>
2022-09-13 04:03:04 +0100

    added about page

---
+++ pages/about.vue
@@ -0,0 +1,34 @@
+<template>
+    
+
+    <div>
+        <h1>Hello from the about page</h1>
+    </div>
+
+
+</template>
+
+<script>
+    export default {
+
+        head(){
+            return {
+                title: "About the App",
+                meta: [
+                    {
+                        hid: "description",
+                        meta: "description",
+                        content: "Best dad jokes"
+                    }
+                ]
+            }
+        }
+
+    }
+</script>
+
+<style>
+
+
+
+</style>
